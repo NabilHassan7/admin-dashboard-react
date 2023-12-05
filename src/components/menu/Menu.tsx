0b0148bd6bc69e51f9747836703c67aa7d4dbd68
NabilHassan7 <fakrulibnehassan@gmail.com>
2023-12-05 06:02:45 +0600

    created vertical menu for the homepage

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,11 +1,29 @@
 // importing CSS file
+import './menu.scss'
+
+// importing from react router dom
+import { Link } from "react-router-dom";
+
+// importing the menu data from data.ts
+import { menu } from '../../data';
 
 const Menu = () => {
     return (
-        <div className="menu">
-            Menu  
-        </div>
+      <div className="menu">
+        {/* mapping the menu items from the data.ts file */}
+        {menu.map((item) => (
+          <div className="item" key={item.id}>
+            <span className="title">{item.title}</span>
+            {item.listItems.map((listItem) => (
+              <Link to={listItem.url} className="listItem" key={listItem.id}>
+                <img src={listItem.icon} alt="" />
+                <span className="listItemTitle">{listItem.title}</span>
+              </Link>
+            ))}
+          </div>
+        ))}
+      </div>
     );
-};
+  };
 
 export default Menu;
